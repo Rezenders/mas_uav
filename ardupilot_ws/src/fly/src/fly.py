3e0a59970d844e2aa9bc0cde2e284591b1bc4614
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2018-08-29 15:56:40 -0300

    ✨ Arming motors

--- ardupilot_ws/src/fly/src/fly.py
+++ ardupilot_ws/src/fly/src/fly.py
@@ -2,7 +2,7 @@
 
 import rospy
 import mavros
-from mavros_msgs.srv import SetMode
+from mavros_msgs.srv import SetMode, CommandBool
 
 if __name__ == '__main__':
     rospy.init_node('fly')
@@ -10,4 +10,8 @@ if __name__ == '__main__':
     rospy.wait_for_service('/mavros/set_mode')
     set_mode = rospy.ServiceProxy('/mavros/set_mode', SetMode)
     set_mode(custom_mode='GUIDED')
+    
+    rospy.wait_for_service('mavros/cmd/arming')
+    arm_motors = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
+    arm_motors(True)
 
