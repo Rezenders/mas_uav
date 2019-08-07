0e5010a96014dd5dab9ed816920ac967072074b1
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2019-08-07 16:22:21 -0300

    ✨ updating yellow_pages

--- comm/yellow_pages.py
+++ comm/yellow_pages.py
@@ -1,14 +1,10 @@
 agents_ip = dict()
 
+# agents_ip["null"] = ['150.162.53.255', 1024]
+# agents_ip["null"] = ['<broadcast>', 1024]
 agents_ip["self"] = ['comm', 1024]
-agents_ip["droneA"] = ['150.162.53.67', 1024]
+agents_ip["droneA"] = ['150.162.53.63', 1024]
 agents_ip["droneB"] = ['150.162.53.71', 1024]
-
-# IP_LISTEN = 'comm'
-# LISTEN_PORT =1024
-#
-# droneA = 150.162.53.67
-# droneA_port = 1024
-#
-# droneB = 150.162.53.68
-# dronB_port = 1024
+agents_ip["scout"] = ['150.162.53.63', 1024]
+agents_ip["rescuer"] = ['150.162.53.71', 1024]
+agents_ip["rescuer2"] = ['150.162.53.78', 1024]
